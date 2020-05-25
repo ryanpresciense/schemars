357dca25b773792b5f1edbb1d7ed06a997b8f5c6
Ryan Roberts <ryansroberts@gmail.com>
2020-05-25 12:58:42 +0100

    Implement on 6,8

--- schemars/src/json_schema_impls/macaddr.rs
+++ schemars/src/json_schema_impls/macaddr.rs
@@ -1,9 +1,42 @@
 use crate::gen::SchemaGenerator;
 use crate::schema::*;
 use crate::JsonSchema;
-use macaddr::MacAddr;
+use macaddr::*;
 
 impl JsonSchema for MacAddr {
+    no_ref_schema!();
+
+    fn schema_name() -> String {
+        "MacAddr".to_string()
+    }
+
+    fn json_schema(_: &mut SchemaGenerator) -> Schema {
+        SchemaObject {
+            instance_type: Some(InstanceType::String.into()),
+            ..Default::default()
+        }
+        .into()
+    }
+}
+
+impl JsonSchema for MacAddr6 {
+    no_ref_schema!();
+
+    fn schema_name() -> String {
+        "MacAddr".to_string()
+    }
+
+    fn json_schema(_: &mut SchemaGenerator) -> Schema {
+        SchemaObject {
+            instance_type: Some(InstanceType::String.into()),
+            ..Default::default()
+        }
+        .into()
+    }
+}
+
+
+impl JsonSchema for MacAddr8 {
     no_ref_schema!();
 
     fn schema_name() -> String {
